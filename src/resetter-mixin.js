0ad0e24ed7176767242e348fcbcec0c5805613dc
Aarón Montes <aaron@kssco.com>
2021-06-15 15:19:34 -0600

    Adding function to clone in deep the $data object

--- src/resetter-mixin.js
+++ src/resetter-mixin.js
@@ -4,8 +4,26 @@ const Resetter = {
   }),
   methods: {
     initResetPlugin() {
-      this.originalData = Object.assign({}, this.$data);
+      this.originalData = this.$clone(this.$data);
       delete this.originalData.originalData;
+    },
+    $clone(object) {
+      const cloning = {};
+      Object.keys(object).map(prop => {
+        if(prop === 'originalData')
+          return;
+        const obj = this.$data[prop];
+        if(Array.isArray(obj)) {
+          cloning[prop] = [...obj]
+        } else if(typeof obj === 'object') {
+          cloning[prop] = this.$clone(obj)
+        } else if (obj instanceof Date) {
+          cloning[prop] = new Date(obj.getTime());
+        } else if (typeof obj === 'function') {
+          cloning[prop] = obj.bind();
+        } else cloning[prop] = obj
+      });
+      return cloning
     },
     resetData(keys) {
       if(keys == null) {
