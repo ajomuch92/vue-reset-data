4ecb5136690110c63214acf397f585e22ad99fe4
Aarón Montes <aaron@kssco.com>
2021-06-16 05:26:23 -0600

    Updating cloning function when a prop is null or undefined

--- src/resetter-mixin.js
+++ src/resetter-mixin.js
@@ -13,15 +13,19 @@ const Resetter = {
         if(prop === 'originalData')
           return;
         const obj = this.$data[prop];
-        if(Array.isArray(obj)) {
-          cloning[prop] = [...obj]
+        if(obj === null) {
+          cloning[prop] = null;
+        } else if(obj === undefined) {
+          cloning[prop] = undefined;
+        } else if(Array.isArray(obj)) {
+          cloning[prop] = [...obj];
         } else if(typeof obj === 'object') {
-          cloning[prop] = this.$clone(obj)
+          cloning[prop] = this.$clone(obj);
         } else if (obj instanceof Date) {
           cloning[prop] = new Date(obj.getTime());
         } else if (typeof obj === 'function') {
           cloning[prop] = obj.bind();
-        } else cloning[prop] = obj
+        } else cloning[prop] = obj;
       });
       return cloning
     },
